af2eeb32f0b09f1e89cefe2949c1eb5da8ebbfff
NatiLucca <33187486+NatiLucca@users.noreply.github.com>
2020-09-13 22:18:42 -0300

    Add files via upload
    Atualizar arquivos

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 if [ ! -d "Resultados" ]; then
@@ -12,27 +13,58 @@ g++ -std=c++11 -o mochilaMatriz mochilaMatriz.cpp
 
 #var="valorMaximo ; tempoExecucao; mochilaMax; numItens"
 
-./mochilaVetor Mochilas/mochila0.txt  >> Resultados/vetor.txt
-./mochilaVetor Mochilas/mochila1.txt >> Resultados/vetor.txt
-./mochilaVetor Mochilas/mochila2.txt >> Resultados/vetor.txt
-./mochilaVetor Mochilas/mochila3.txt >> Resultados/vetor.txt
-./mochilaVetor Mochilas/mochila4.txt >> Resultados/vetor.txt
-./mochilaVetor Mochilas/mochila5.txt  >> Resultados/vetor.txt
-./mochilaVetor Mochilas/mochila6.txt >> Resultados/vetor.txt
-./mochilaVetor Mochilas/mochila7.txt >> Resultados/vetor.txt
-./mochilaVetor Mochilas/mochila8.txt >> Resultados/vetor.txt
-./mochilaVetor Mochilas/mochila9.txt >> Resultados/vetor.txt
+for i in $(seq 1 11);
+do
+	#./mochilaVetor Mochilas/mochila0.txt  >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila1.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila2.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila3.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila4.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila5.txt  >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila6.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila7.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila8.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila9.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila10.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila11.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila12.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila13.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila14.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila15.txt >> Resultados/vetor.txt
+	#./mochilaVetor Mochilas/mochila16.txt >> Resultados/vetor.txt
+	./mochilaVetor Mochilas/mochila17.txt >> Resultados/vetor.txt
+	./mochilaVetor Mochilas/mochila18.txt >> Resultados/vetor.txt
+	./mochilaVetor Mochilas/mochila19.txt >> Resultados/vetor.txt
 
 
-./mochilaMatriz Mochilas/mochila0.txt  >> Resultados/matriz.txt
-./mochilaMatriz Mochilas/mochila1.txt >> Resultados/matriz.txt
-./mochilaMatriz Mochilas/mochila2.txt >> Resultados/matriz.txt
-./mochilaMatriz Mochilas/mochila3.txt >> Resultados/matriz.txt
-./mochilaMatriz Mochilas/mochila4.txt >> Resultados/matriz.txt
-./mochilaMatriz Mochilas/mochila5.txt  >> Resultados/matriz.txt
-./mochilaMatriz Mochilas/mochila6.txt >> Resultados/matriz.txt
-./mochilaMatriz Mochilas/mochila7.txt >> Resultados/matriz.txt
-./mochilaMatriz Mochilas/mochila8.txt >> Resultados/matriz.txt
-./mochilaMatriz Mochilas/mochila9.txt >> Resultados/matriz.txt
+done
+
+for i in $(seq 1 11);
+do
+	#./mochilaMatriz Mochilas/mochila0.txt  >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila1.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila2.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila3.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila4.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila5.txt  >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila6.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila7.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila8.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila9.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila10.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila11.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila12.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila13.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila14.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila15.txt >> Resultados/matriz.txt
+	#./mochilaMatriz Mochilas/mochila16.txt >> Resultados/matriz.txt
+	./mochilaMatriz Mochilas/mochila17.txt >> Resultados/matriz.txt
+	./mochilaMatriz Mochilas/mochila18.txt >> Resultados/matriz.txt
+	./mochilaMatriz Mochilas/mochila19.txt >> Resultados/matriz.txt
+done
+
+
+rm -r mochilaMatriz
+rm -r mochilaVetor
 
 exit
--- runGerarMochilas.sh
+++ runGerarMochilas.sh
@@ -6,34 +6,42 @@ g++ -std=c++11 -o mochila geraMochila.cpp
 
 # mochila 0 - 1000 100
 # mochila 1 - 10000 100
-./mochila Mochilas/mochila2.txt 100000 500 100000 10000
 
-./mochila Mochilas/mochila3.txt 200000 500 100000 10000
+#./mochila Mochilas/mochila2.txt 100000 500 100000 10000
 
-./mochila Mochilas/mochila4.txt 300000 800 100000 10000
+#./mochila Mochilas/mochila3.txt 200000 500 100000 10000
 
-./mochila Mochilas/mochila5.txt 400000 800 100000 10000
+#./mochila Mochilas/mochila4.txt 300000 800 100000 10000
 
-./mochila Mochilas/mochila6.txt 500000 1000 100000 10000
+#./mochila Mochilas/mochila5.txt 400000 800 100000 10000
 
-./mochila Mochilas/mochila7.txt 600000 1000 100000 10000
+#./mochila Mochilas/mochila6.txt 500000 1000 100000 10000
 
-./mochila Mochilas/mochila8.txt 700000 1200 100000 10000
+#./mochila Mochilas/mochila7.txt 600000 1000 100000 10000
 
-./mochila Mochilas/mochila9.txt 800000 1200 100000 10000
+#./mochila Mochilas/mochila8.txt 700000 1200 100000 10000
 
-./mochila Mochilas/mochila10.txt 900000 1500 100000 10000
+#./mochila Mochilas/mochila9.txt 800000 1200 100000 10000
 
-./mochila Mochilas/mochila11.txt 1000000 1500 100000 10000
+#./mochila Mochilas/mochila10.txt 900000 1500 100000 10000
+
+#./mochila Mochilas/mochila11.txt 1000000 1500 100000 10000
+
+#./mochila Mochilas/mochila14.txt 2000000 1500 100000 10000
+
+#./mochila Mochilas/mochila15.txt 2000000 2000 100000 10000
+
+#./mochila Mochilas/mochila16.txt 2500000 2500 100000 10000
 
 # mochila 12 - 2000000 500
 # mochila 13 - 2000000 500
 
-./mochila Mochilas/mochila14.txt 2000000 1500 100000 10000
+./mochila Mochilas/mochila17.txt 3000000 3000 100000 10000
 
-./mochila Mochilas/mochila15.txt 2000000 2000 100000 10000
+./mochila Mochilas/mochila18.txt 3500000 3500 100000 10000
 
-./mochila Mochilas/mochila16.txt 2500000 2500 100000 10000
+./mochila Mochilas/mochila19.txt 5000000 5000 100000 10000
+
 
 
 rm -r mochila
